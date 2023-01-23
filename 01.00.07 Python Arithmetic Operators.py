36cb591ff991ca3e0f623a10290ebfa0b85dc6cb
Connor-Hart <chart1@ualr.edu>
2023-01-23 15:38:26 +0000

    Instruction Programs

---
+++ 01.00.07 Python Arithmetic Operators.py
@@ -0,0 +1,10 @@
+x = 11
+y = 4
+
+print (x + y) #addition
+print (x - y) #subtraction
+print (x * y) #multiplication
+print (x / y) #division
+print (x % y) #modulus
+print (x ** y) #exponentation
+print (x // y) #floor division
---
+++ 01.00.09 Inputting Data.py
@@ -0,0 +1,5 @@
+  # Input statement without a prompt
+print("You Entered: " + (input()))
+
+  # Input statement with a prompt
+print("You Entered: " + (input("Enter Your Name:")))
